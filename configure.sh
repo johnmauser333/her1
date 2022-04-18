67bcb339f6a98587e8bd2c2af089d24783cd1fa3
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-19 01:28:24 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,94 +14,30 @@ rm -rf /tmp/v2ray
 install -d /usr/local/etc/v2ray
 cat << EOF > /usr/local/etc/v2ray/config.json
 {
-// reverse proxy portal
-  "reverse": {
-    "portals": [
-      {
-        "tag": "portal",
-        "domain": "apacheapache.com.jp"  // the same as bridge
-      }
-    ]
-  },
-
-// v2ray + ws + tls config
-  "inbounds": [
-  // receive client's connection
-  {
-    "tag": "clientin",
-    "port": $PORT,
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
+    "inbounds": [
         {
-          "id": "$UUID",
-          "alterId": 0
+            "port": $PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "security": "chacha20-poly1305",
+                        "alterId": 0
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
         }
-      ]
-    },
-    "streamSettings": {
-      "network": "ws",
-      "wsSettings": {
-        "path": "/v2ray"
-      }
-    }
-  },
-
-// receive bridge's connection
-  {
-    "tag": "interconn",
-    "port": $PORT,
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
+    ],
+    "outbounds": [
         {
-          "id": "$UUID",
-          "alterId": 0
+            "protocol": "freedom"
         }
-      ]
-    },
-    "streamSettings": {
-      "network": "ws",
-      "wsSettings": {
-        "path": "/path"
-      }
-    }  
-  }
-
-], // end of the inbounds
-
-
-// outbounds for network proxy
-  "outbounds": [{
-    "tag": "crossfire",
-    "protocol": "freedom",
-    "settings": {}
-  }],
-
-// routing rules
-  "routing": {
-    "rules": [
-
-      {
-        "type": "field",
-        "inboundTag": ["interconn"],
-        "outboundTag": "portal"
-      },
-
-      {
-        "type": "field",
-        "inboundTag": ["clientin"],
-        "port": "$PORT",
-        "outboundTag": "portal"  // for a specific ip and port range to access remote services
-      },
-
-      {
-        "type": "field",
-        "inboundTag": ["clientin"],
-        "outboundTag": "crossfire"  // remaining traffic goes here
-      }
     ]
-  }
 }
 EOF
 

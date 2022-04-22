e8c27363960d373db820ce1fe7df70f8bfaa957b
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-23 00:09:30 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 # Download and install V2Ray
-mkdir /tmp/v2ray
-wget -q https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -O /tmp/v2ray/v2ray.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+mkdir /tmp/xray
+wget -q https://github.com/XTLS/Xray-core/releases/download/v1.5.4/Xray-linux-64.zip -O /tmp/v2ray/xray.zip
+unzip /tmp/v2ray/xray.zip -d /tmp/xray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/xray
 
 # Remove temporary directory
-rm -rf /tmp/v2ray
+rm -rf /tmp/xray
 
 # V2Ray new configuration
-install -d /usr/local/etc/v2ray
-cat << EOF > /usr/local/etc/v2ray/config.json
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
 {
 // reverse proxy portal
   "reverse": {
@@ -100,4 +99,4 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 EOF
 
 # Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json

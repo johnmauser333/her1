f2d0c476dc6b1b81fb886176cfaa062b7d5053a9
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-23 01:37:03 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -84,7 +84,7 @@ cat << EOF > /usr/local/etc/xray/config.json
       {
         "type": "field",
         "inboundTag": ["clientin"],
-        "ip": "192.168.50.50",
+        "domain": "playstation333.herokuapp.com",
         "port": "80,443",
         "outboundTag": "portal"  // for a specific ip and port range to access remote services
       },

d7c1e765a4873be1d18502e4e146c346fc3cef36
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-24 23:27:54 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -19,8 +19,8 @@ cat << EOF > /usr/local/etc/xray/config.json
             "port": $PORT,
             "protocol": "dokodemo-door",
             "settings": {
-                "address": "127.0.0.1",
-                "port": 8080,
+                "address": "0.0.0.0",
+                "port": 443,
                 "network": "tcp"
             }
         },

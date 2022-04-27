2bca970aec54ef33182a0cd80a5833c4d8a059f2
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-28 01:52:35 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,45 @@
 #!/bin/sh
-npm install wstunnel
-wstunnel -s $PORT
+
+# Download and install V2Ray
+mkdir /tmp/v2ray
+wget -q https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -O /tmp/v2ray/v2ray.zip
+unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+
+# Remove temporary directory
+rm -rf /tmp/v2ray
+
+# V2Ray new configuration
+install -d /usr/local/etc/v2ray
+cat << EOF > /usr/local/etc/v2ray/config.json
+{
+    "inbounds": [
+        {
+            "port": 443,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "ac7508a0-4bb4-4ba1-b30e-cc3b7df6558e",
+                        "security": "chacha20-poly1305",
+                        "alterId": 0
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+# Run V2Ray
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
